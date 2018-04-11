b32a3f6008a0d04b2ad376c9f4d5c3ea67d99fa1
Judson Stevens <Judson.Stevens@gmail.com>
2018-04-11 09:10:33 -0600

    Iteration 1 and tests done

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,12 @@
+# This class holds information about each Book object.
 class Book
-
-
+  attr_reader :book_information
+  def initialize(book_information)
+    @book_information = {
+      author_first_name:     book_information[:author_first_name],
+      author_last_name:      book_information[:author_last_name],
+      title:                 book_information[:title],
+      publication_date:      book_information[:publication_date]
+    }
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,41 @@
 require './test/test_helper'
+require './lib/book.rb'
 
 class BookTest < Minitest::Test
+  def setup
+    @b = Book.new(author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960")
+  end
 
+  def test_it_exists
+    assert_instance_of Book, @b
+  end
+
+  def test_it_can_return_first_name
+    expected = 'Harper'
+    actual = @b.book_information[:author_first_name]
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_return_last_name
+    expected = 'Lee'
+    actual = @b.book_information[:author_last_name]
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_return_title
+    expected = 'To Kill a Mockingbird'
+    actual = @b.book_information[:title]
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_return_publication_date
+    expected = 'July 11, 1960'
+    actual = @b.book_information[:publication_date]
+
+    assert_equal expected, actual
+  end
 
 end
